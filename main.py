106c32693f706508b6700b4325fbea0967e2225f
Kailey Liou <kaileyliou09@gmail.com>
2026-09-11 12:29:13 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((600, 400))
+pygame.display.set_caption("My first pygame!")
+clock = pygame.time.Clock() # tracks time and frame rate
+
+x, y, dx, dy, r = 300, 200, 4, 3, 24 # initializing everything
+running = True
+
+while running: # game loop
+    for event in pygame.event.get(): # to get every single event
+        if event.type == pygame.QUIT:
+            running = False
+    
+    if x - r < 0 or x + r > 600:
+        dx = -dx
+
+    if y - r < 0 or y + r > 400:
+        dy = -dy
+
+    screen.fill(20, 24, 40)
+    pygame.draw.circle(screen, (233, 30, 99), (x,y), r)
+    pygame.display.flip()
+    
+
+pygame.quit()
