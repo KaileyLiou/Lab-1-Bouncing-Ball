adffca71831cb13fc31716dcef5032a4d06b4488
Kailey Liou <kaileyliou09@gmail.com>
2026-09-13 22:26:57 -0400

    part 1 complete

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ while running: # game loop
     for event in pygame.event.get(): # to get every single event
         if event.type == pygame.QUIT:
             running = False
+
+    x += dx
+    y += dy
     
     if x - r < 0 or x + r > 600:
         dx = -dx
@@ -18,9 +21,10 @@ while running: # game loop
     if y - r < 0 or y + r > 400:
         dy = -dy
 
-    screen.fill(20, 24, 40)
+    screen.fill((20, 24, 40))
     pygame.draw.circle(screen, (233, 30, 99), (x,y), r)
     pygame.display.flip()
-    
+
+    clock.tick(60)
 
 pygame.quit()
